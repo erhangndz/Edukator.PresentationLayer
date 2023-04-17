3313499f834bda0956daf6f114b90415af46e113
erhangndz <erhangndz@live.com>
2023-04-17 22:57:38 +0300

    updated

---
+++ Edukator.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ERHAN\\SQLEXPRESS;initial catalog=DbEdukator;integrated security=true;");
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Category> Categories  { get; set; }
+
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
