9194981e5ecf739f5fcc513f080189e89cdcd2af
erhangndz <erhangndz@live.com>
2023-04-25 14:38:55 +0300

    updated

--- Edukator.PresentationLayer/Controllers/CourseController.cs
+++ Edukator.PresentationLayer/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using Edukator.BusinessLayer.Abstract;
+using Edukator.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Edukator.PresentationLayer.Controllers
@@ -16,6 +17,38 @@ namespace Edukator.PresentationLayer.Controllers
         {
             var values = _courseService.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCourse(Course p)
+        {
+            _courseService.TInsert(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCourse(int id)
+        {
+            var values = _courseService.TGetByID(id);
+            _courseService.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCourse(int id)
+        {
+            var values = _courseService.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCourse(Course p)
+        {
+            _courseService.TUpdate(p);
+            return RedirectToAction("Index");
         }
     }
 }
